d3bd6419616299e4c8805a607fefe292eb1e7bbc
Moeez1999 <moeez.suleman460@gmail.com>
2021-10-13 23:09:41 +0500

    App in Deployment Phase

--- lib/widgets/navbar.dart
+++ lib/widgets/navbar.dart
@@ -11,7 +11,9 @@
 //   @override
 //   _NavBarState createState() => _NavBarState();
 // }
+   
 
+   
 // class _NavBarState extends State<NavBar> {
 //   @override
 //   Widget build(BuildContext context) {
